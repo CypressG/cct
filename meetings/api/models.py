fcb0977ccc3e553f022aff939ee43b70c5bc6274
cypressg <krasauskas.kipras@gmail.com>
2022-01-29 20:01:17 +0200

    Added models, admin, settings and urls

--- meetings/api/models.py
+++ meetings/api/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+
+
+class User(AbstractUser):
+    pass
+
+
+class Registry(models.Model):
+    pass
+
+
+class Group(models.Model):
+    pass
+
+
+class Meeting(models.Model):
+    pass
